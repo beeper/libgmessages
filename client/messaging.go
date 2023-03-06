061f78048712aa38f529dae00f785fca396a3d9d
Gary Kramlich <grim@reaperworld.com>
2023-03-06 00:46:52 -0600

    Add some missing headers to the messaging requests
    The messaging endpoint was returning a 404 for a known endpoint, turns out we
    were just missing some headers so it was refusing to serve it.

--- client/messaging.go
+++ client/messaging.go
@@ -14,15 +14,16 @@ func (client *Client) messagingHeaders() http.Header {
 	h.Set("Authority", headerAuthority)
 	h.Set("Origin", headerOrigin)
 	h.Set("Referer", headerReferer)
+	h.Set("Content-Type", "application/json+protobuf")
 	h.Set("Sec-Fetch-Dest", headerSecFetchDest)
 	h.Set("Sec-Fetch-Mode", headerSecFetchMode)
 	h.Set("Sec-Fetch-Site", headersSecFetchSite)
 	h.Set("TE", headerTE)
 	h.Set("User-Agent", headerUserAgent)
 	h.Set("X-User-Agent", headerXUserAgent)
+	h.Set("x-goog-api-key", googleAPIKey)
 
 	return h
-
 }
 
 func (client *Client) messagingRequest(message proto.Message, endpoint string) (*http.Request, error) {
